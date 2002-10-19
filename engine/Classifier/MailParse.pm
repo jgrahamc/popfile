73616911a1f9f5603290d1f17584094d4dee28ad
John Graham-Cumming <popfile@jgc.org>
2002-10-19 20:15:38 +0000

    Speeded up base64 decoding; added quoted-printable support; fixed bugs 625017 and 625019 by quoting regexp metacharacters in the mime boundary; add extractors for email addresses and domain names; improved accuracy of word matching; added removal of HTML tags if content is HTML
    git-svn-id: file:///usr/local/svn/trunk@32 215d8e1a-9f62-4c3a-9943-246481b8efb3

--- engine/Classifier/MailParse.pm
+++ engine/Classifier/MailParse.pm
@@ -12,7 +12,7 @@ use Classifier::WordMangle;
 #----------------------------------------------------------------------------
 # new
 #
-#   Class new() function
+# Class new() function
 #----------------------------------------------------------------------------
 
 sub new
@@ -21,15 +21,19 @@ sub new
     my $self;
 
     # Used to mangle words into the right shape for classification
+
     $self->{mangle} = new Classifier::WordMangle;
   
     # Hash of word frequences
+
     $self->{words}  = {};
 
     # Total word cout
+
     $self->{msg_total} = 0;
     
     # Debug messages?
+
     $self->{debug}     = 0;
     
     return bless $self, $type;
@@ -46,18 +50,15 @@ sub new
 sub un_base64
 {
     my ($self, $line) = @_;
-    
-    $line =~ tr|A-Za-z0-9+=/||cd;
-    
-    if ( length( $line ) % 4 ) 
-    {
-        return $line;
-    }
+    my $result;
     
     $line =~ s/=+$//; 
     $line =~ tr|A-Za-z0-9+/| -_|;
 
-    return join'', map( unpack("u", chr(32 + length($_)*3/4) . $_), $line =~ /(.{1,60})/gs);
+    $result = join'', map( unpack("u", chr(32 + length($_)*3/4) . $_), $line =~ /(.{1,60})/gs);
+    $result =~ s/\x00//g;
+    
+    return $result;
 }
 
 # ---------------------------------------------------------------------------------------------
@@ -79,7 +80,7 @@ sub update_word
         $self->{words}{$word} += 1;
         $self->{msg_total}    += 1;
         
-        print "Updated word $word to $self->{words}{$word}\n" if ($self->{debug});
+        print "--- $word ($self->{words}{$word})\n" if ($self->{debug});
     }
 }
 
@@ -94,12 +95,26 @@ sub update_word
 sub add_line
 {
     my ($self, $line) = @_;
-    
+
+    # Pull out any email addresses in the line that are marked with <> and have an @ in them
+
+    while ( $line =~ s/<([A-Za-z0-9\-_]+?@[A-Za-z0-9\-_\.]+?)>// ) 
+    {
+        update_word($self, $1);
+    }
+
+    # Grab domain names
+
+    while ( $line =~ s/(([A-Za-z][A-Za-z0-9\-_]+\.){2,})([A-Za-z0-9\-_]+)([^A-Za-z0-9\-_]|$)/\4/ ) 
+    {
+        update_word($self, "$1$3");
+    }
+
     # Only care about words between 3 and 45 characters since short words like
     # an, or, if are too common and the longest word in English (according to
     # the OED) is pneumonoultramicroscopicsilicovolcanoconiosis
 
-    while ( $line =~ s/([A-Za-z]{3,45})[^0-9]// )
+    while ( $line =~ s/([A-Za-z][A-Za-z\']{2,44})[,\.\"\'\)\?!]{0,2}([ \t\n\r]|$)// )
     {
         update_word($self,$1);
     }
@@ -118,12 +133,15 @@ sub parse_stream
     my ($self, $file) = @_;
     
     # This will contain the mime boundary information in a mime message
+
     my $mime     = '';
     
     # Contains the encoding for the current block in a mime message
+
     my $encoding = '';
 
     # Clear the word hash
+
     $self->{words}     = {};
     $self->{msg_total} = 0;
 
@@ -136,8 +154,17 @@ sub parse_stream
     {
         my $line = $_;
     
-        print $line if $self->{debug};
+        print ">>> $line" if $self->{debug};
 
+        # If we are in a mime document then spot the boundaries
+
+        if ( ( $mime ne '' ) && ( $line =~ /$mime/ ) )
+        {
+            print "Hit mime boundary\n" if $self->{debug};
+            $encoding = '';
+            next;
+        }
+    
         # If we are doing base64 decoding then look for suitable lines and remove them 
         # for decoding
     
@@ -146,22 +173,29 @@ sub parse_stream
             my $decoded = '';
             while ( ( $line =~ /^[A-Za-z0-9+\/]{72}[\n\r]?$/ ) || ( $line =~ /^[A-Za-z0-9+\/]+=?[\n\r]?$/ ) )
             {
+                print "64> $line" if $self->{debug};
                 $decoded .= un_base64( $self, $line );
+                if ( $decoded =~ /[^A-Za-z\-\.]$/ ) 
+                {
+                    add_line( $self, $decoded );
+                    $decoded = '';
+                }
                 $line = <MSG>;
             }
             
             add_line( $self, $decoded );
         }
 
-        # If we are in a mime document then spot the boundaries
-        my $re = qr/$mime/;
-        if ( ( $mime ne '' ) && ( $line =~ /$re/ ) )
+        # Remove HTML tags completely
+        
+        if ( $encoding =~ /quoted\-printable/ ) 
         {
-            print "Hit mime boundary\n" if $self->{debug};
-            $encoding = '';
-            next;
+            $line =~ s/=20/ /g;
+            $line =~ s/<[\/!]?[A-Za-z]+[^>]*?>//g;
+            $line =~ s/<[\/!]?[A-Za-z]+[^>]*?$//;
+            $line =~ s/^[\/!]?[A-Za-z]+[^>]*?>//;
         }
-    
+        
         # If we have an email header then just keep the part after the :
         
         if ( $line =~ /^([A-Za-z-]+): ([^\n\r]*)/ ) 
@@ -187,26 +221,30 @@ sub parse_stream
                 }
                 
                 add_line( $self, $argument );
-                
                 next;
             }
             
             # Look for MIME
             
-            if ( ( $header =~ /Content-Type/i ) && ( $argument =~ /multipart\//i ) )
+            if ( $header =~ /Content-Type/i ) 
             {
-                my $boundary = $argument;
-                if ( !( $argument =~ /boundary=\"(.*)\"/ ))
+                if ( $argument =~ /multipart\//i )
                 {
-                    $boundary = <MSG>;
+                    my $boundary = $argument;
+                    if ( !( $argument =~ /boundary=\"(.*)\"/ ))
+                    {
+                        $boundary = <MSG>;
+                    }
+
+                    if ( $boundary =~ /boundary=\"(.*)\"/ ) 
+                    {
+                        print "Set mime boundary to $1\n" if $self->{debug};
+                        
+                        $mime = $1;
+                        $mime =~ s/(\+|\/|\?|\*|\||\(|\)|\[|\]|\{|\}|\^|\$|\.)/\\\1/g;
+                    }
                 }
-                
-                if ( $boundary =~ /boundary=\"(.*)\"/ ) 
-                {
-                    print "Set mime boundary to $1\n" if $self->{debug};
-                    $mime = $1;
-                }
-                
+
                 next;
             }
             
@@ -216,11 +254,11 @@ sub parse_stream
             if ( $header =~ /Content-Transfer-Encoding/i )
             {
                 $encoding = $argument;
-                
                 next;
             }
 
             # Some headers to discard
+
             if ( $header =~ /(Thread-Index|X-UIDL|Message-ID|X-Text-Classification)/i ) 
             {
                 next;
