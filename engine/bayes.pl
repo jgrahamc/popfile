944846ac40a72d384348e0ea4e32cf2d278ad391
John Graham-Cumming <popfile@jgc.org>
2002-10-20 01:55:03 +0000

    Addition of small test script
    git-svn-id: file:///usr/local/svn/trunk@40 215d8e1a-9f62-4c3a-9943-246481b8efb3

---
+++ engine/bayes.pl
@@ -0,0 +1,30 @@
+# ---------------------------------------------------------------------------------------------
+#
+# bayes.pl --- Classify a mail message manually
+#
+# ---------------------------------------------------------------------------------------------
+
+use strict;
+use Classifier::Bayes;
+
+# main
+
+if ( $#ARGV == 0 ) 
+{
+    my $b = new Classifier::Bayes;
+    
+    $b->{debug} = 1; 
+    $b->load_word_matrix();
+
+    my @files   = glob $ARGV[0];
+    foreach my $file (@files)
+    {
+        print "$file is '" . $b->classify_file($file) . "'\n";
+    }
+}
+else
+{
+    print "bayes.pl - output the probability that a message is in each bucket\n\n";
+    print "Usage: bayes.pl <messages>\n";
+    print "       <messages>         Filename of message(s) to classify\n";
+}
