6142352f093e36397cbf79c6fae0d3e1438ff695
John Graham-Cumming <popfile@jgc.org>
2002-10-20 18:54:10 +0000

    Small test script for checking the accuracy of the classifier
    git-svn-id: file:///usr/local/svn/trunk@48 215d8e1a-9f62-4c3a-9943-246481b8efb3

---
+++ engine/autotest.pl
@@ -0,0 +1,52 @@
+# ---------------------------------------------------------------------------------------------
+#
+# autotest.pl --- Used to test classification accuracy
+#
+# ---------------------------------------------------------------------------------------------
+
+use strict;
+use Classifier::Bayes;
+
+# main
+
+if ( $#ARGV == -1 )
+{
+    my $total = 0;
+    my $ok    = 0;
+    my $fail  = 0;
+    
+    my $b = new Classifier::Bayes;
+    $b->load_word_matrix();
+    
+    my @tests = glob "test/*";
+
+    foreach my $test (@tests)
+    {
+        $test =~ /([A-Za-z0-9-_]+)$/;
+        my $class = $1;
+        my @files = glob "$test/*";
+        
+        for my $file (@files )
+        {
+            $total += 1;
+            my $classification = $b->classify_file($file);
+            
+            if ( $classification eq $class ) 
+            {
+                $ok   += 1;
+            }
+            else
+            {
+                print "\n$file should be in $class but is in $classification"; 
+                $fail += 1;
+            }
+        }   
+    }
+    
+    print "\nTotal tests: $total, successful: $ok, failed: $fail\n";
+}
+else
+{
+    print "autotest.pl - test classification accuracy\n\n";
+    print "Usage: autotest.pl\n";
+}
