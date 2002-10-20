37d860e172729ba59ec6f5f7761c53079b55a987
John Graham-Cumming <popfile@jgc.org>
2002-10-20 01:33:15 +0000

    New script for email coloring
    git-svn-id: file:///usr/local/svn/trunk@38 215d8e1a-9f62-4c3a-9943-246481b8efb3

---
+++ engine/color.pl
@@ -0,0 +1,27 @@
+# ---------------------------------------------------------------------------------------------
+#
+# color.pl --- Used to view a colorized email message
+#
+# ---------------------------------------------------------------------------------------------
+
+use strict;
+use Classifier::MailParse;
+use Classifier::Bayes;
+
+# main
+
+if ( $#ARGV == 0 ) 
+{
+    my $m = new Classifier::MailParse;
+    
+    $m->{bayes} = new Classifier::Bayes;
+    $m->{bayes}->load_word_matrix();
+    $m->{color} = 1;
+    $m->parse_stream($ARGV[0]);
+}
+else
+{
+    print "color.pl - color an email by word bucket\n\n";
+    print "Usage: color.pl <msg>\n";
+    print "       <msg>    The message to color\n";
+}
