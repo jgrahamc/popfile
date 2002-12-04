9330ed0e8f142c0d6d8ff8ae12fea1e6a18b86c2
John Graham-Cumming <popfile@jgc.org>
2002-12-04 00:43:20 +0000

    Bump version number
    git-svn-id: file:///usr/local/svn/trunk@235 215d8e1a-9f62-4c3a-9943-246481b8efb3

--- engine/popfile.pl
+++ engine/popfile.pl
@@ -23,7 +23,7 @@ use IO::Select;
 # This version number
 my $major_version = 0;
 my $minor_version = 17;
-my $build_version = 7;
+my $build_version = 8;
 
 # The name of the debug file
 my $debug_filename;
