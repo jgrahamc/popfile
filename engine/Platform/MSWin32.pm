5621123d40d7256343aa2b6b8afa479254864227
John Graham-Cumming <popfile@jgc.org>
2003-06-29 20:59:59 +0000

    Handle new option to hide the system tray icon from the menu, and add configuration item that makes the tray icon configurable from the web interface
    git-svn-id: file:///usr/local/svn/trunk@975 215d8e1a-9f62-4c3a-9943-246481b8efb3

--- engine/Platform/MSWin32.pm
+++ engine/Platform/MSWin32.pm
@@ -49,7 +49,84 @@ sub initialize
 
     $self->config_( 'trayicon', 1 );
 
+    $self->register_configuration_item_( 'configuration',
+                                         'windows_trayicon',
+                                         $self );
+
     return 1;
+}
+
+# ---------------------------------------------------------------------------------------------
+#
+# configure_item
+#
+#    $name            The name of the item being configured, was passed in by the call
+#                     to register_configuration_item
+#    $language        Reference to the hash holding the current language
+#    $session_key     The current session key
+#
+#  Must return the HTML for this item
+# ---------------------------------------------------------------------------------------------
+
+sub configure_item
+{
+    my ( $self, $name, $language, $session_key ) = @_;
+
+    my $body;
+
+    # POP3 Listen Port widget
+    if ( $name eq 'windows_trayicon' ) {
+        $body .= "<span class=\"configurationLabel\">$$language{Windows_TrayIcon}:</span><br />\n";
+        $body .= "<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" summary=\"\"><tr><td nowrap=\"nowrap\">\n";
+
+        if ( $self->config_( 'trayicon' ) == 0 ) {
+            $body .= "<form action=\"/configuration\">\n";
+            $body .= "<span class=\"securityWidgetStateOff\">$$language{No}</span>\n";
+            $body .= "<input type=\"submit\" class=\"toggleOn\" id=\"windowTrayIconOn\" name=\"toggle\" value=\"$$language{ChangeToYes}\" />\n";
+            $body .= "<input type=\"hidden\" name=\"windows_trayicon\" value=\"1\" />\n";
+            $body .= "<input type=\"hidden\" name=\"session\" value=\"$session_key\" />\n</form>\n";
+        } else {
+            $body .= "<form action=\"/configuration\">\n";
+            $body .= "<span class=\"securityWidgetStateOn\">$$language{Yes}</span>\n";
+            $body .= "<input type=\"submit\" class=\"toggleOn\" id=\"windowTrayIconOff\" name=\"toggle\" value=\"$$language{ChangeToNo}\" />\n";
+            $body .= "<input type=\"hidden\" name=\"windows_trayicon\" value=\"0\" />\n";
+            $body .= "<input type=\"hidden\" name=\"session\" value=\"$session_key\" />\n</form>\n";
+        }
+        $body .= "</td></tr></table>\n";
+    }
+
+    return $body;
+}
+
+# ---------------------------------------------------------------------------------------------
+#
+# validate_item
+#
+#    $name            The name of the item being configured, was passed in by the call
+#                     to register_configuration_item
+#    $language        Reference to the hash holding the current language
+#    $form            Hash containing all form items
+#
+#  Must return the HTML for this item
+# ---------------------------------------------------------------------------------------------
+
+sub validate_item
+{
+    my ( $self, $name, $language, $form ) = @_;
+
+    if ( $name eq 'windows_trayicon' ) {
+        if ( defined($$form{windows_trayicon}) ) {
+            $self->config_( 'trayicon', $$form{windows_trayicon} );
+
+            if ( $$form{windows_trayicon} == 0 ) {
+                $self->{hideicon__} = Win32::API->new( "Platform/POPFileIcon.dll", "HideIcon", "", "N" );
+                $self->{hideicon__}->Call();
+                undef $self->{hideicon__};
+	    }
+        }
+    }
+
+    return '';
 }
 
 # ---------------------------------------------------------------------------------------------
@@ -91,6 +168,18 @@ sub service
     }
 
     my $event = $self->{getmessage__}->Call();
+
+    # User wants the icon hidden
+
+    if ( $event == 3 ) {
+        $self->config_( 'trayicon', 0 );
+
+        $self->{hideicon__} = Win32::API->new( "Platform/POPFileIcon.dll", "HideIcon", "", "N" );
+        $self->{hideicon__}->Call();
+        undef $self->{hideicon__};
+
+        return 1;
+    }
 
     # Double click icon, or select Open option in menu results in
     # navigating to the UI
