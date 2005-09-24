aaeabe7ec20c4931463c8d51ccfbdfadaf44a5ad
Manni Heumann <unknown@example.com>
2005-09-24 20:10:41 +0000

    Fix colspan bug on history page the way Brian suggested to fix it.
    git-svn-id: file:///usr/local/svn/trunk@2644 215d8e1a-9f62-4c3a-9943-246481b8efb3

--- engine/UI/HTML.pm
+++ engine/UI/HTML.pm
@@ -3131,8 +3131,10 @@ sub history_page
                  ( $self->user_config_( $self->{sessions__}{$session}{user}, 'session_dividers' ) ) ) {
                 $row_data{History_If_Session} = ( abs( $$row[7] - $last ) >
                                                   300 );
-                $row_data{History_Colspan} = $colspan+1;
             }
+            # we set this here so feedback lines will also 
+            # get the correct colspan:
+            $row_data{History_Colspan} = $colspan+1;
 
             $last = $$row[7];
 
