b4cf0ed39f80712464425c9b695c1fbba605f3f8
Naoki Iimura <unknown@example.com>
2007-11-27 14:46:49 +0000

    Fixed the downloading message link in the single message view.
    git-svn-id: file:///usr/local/svn/trunk@2970 215d8e1a-9f62-4c3a-9943-246481b8efb3

--- engine/UI/HTML.pm
+++ engine/UI/HTML.pm
@@ -3184,7 +3184,7 @@ sub view_page
     # Provide message in plain text for user download/recovery purposes.
 
     if ( defined( $self->{form_}{text} ) ) {
-        $self->http_file_( $client, $self->{history__}->get_slot_file( $self->{form_}{view} ), 'text/plain' );
+        $self->http_file_( $client, $self->history_()->get_slot_file( $self->{form_}{view} ), 'text/plain' );
         return 1;
     }
 
